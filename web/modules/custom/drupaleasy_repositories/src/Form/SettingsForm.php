e47d817d84a9264645215044e521f484eccf44d1
Michael Anello <michael@anelloconsulting.com>
2024-08-29 15:31:53 -0400

    End of class 6.

--- web/modules/custom/drupaleasy_repositories/src/Form/SettingsForm.php
+++ web/modules/custom/drupaleasy_repositories/src/Form/SettingsForm.php
@@ -30,10 +30,18 @@ final class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
+    // Get the current config.
+    $repositories_config = $this->config('drupaleasy_repositories.settings')
+      ->get('repositories_plugins') ?? [];
+
     $form['repositories_plugins'] = [
       '#type' => 'checkboxes',
       '#title' => $this->t('Repository plugins'),
-      '#options' => ['yml_remote' => 'Yml remote'],
+      '#options' => [
+        'yml_remote' => $this->t('Yml remote'),
+        'github' => $this->t('GitHub'),
+      ],
+      '#default_value' => $repositories_config,
     ];
     return parent::buildForm($form, $form_state);
   }
@@ -41,26 +49,9 @@ final class SettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state): void {
-    // @todo Validate the form here.
-    // Example:
-    // @code
-    //   if ($form_state->getValue('example') === 'wrong') {
-    //     $form_state->setErrorByName(
-    //       'message',
-    //       $this->t('The value is not correct.'),
-    //     );
-    //   }
-    // @endcode
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state): void {
     $this->config('drupaleasy_repositories.settings')
-      ->set('example', $form_state->getValue('example'))
+      ->set('repositories_plugins', $form_state->getValue('repositories_plugins'))
       ->save();
     parent::submitForm($form, $form_state);
   }
--- web/modules/custom/drupaleasy_repositories/tests/src/Functional/AddYmlRepoTest.php
+++ web/modules/custom/drupaleasy_repositories/tests/src/Functional/AddYmlRepoTest.php
@@ -48,7 +48,6 @@ final class AddYmlRepoTest extends BrowserTestBase {
     // $this->createRepositoryContentType();
     // Add the Repository URL field to the user entity.
     // $this->createUserRepositoryUrlField();
-
     // Ensure that the Repository URL field is visible in the existing user
     // entity form mode. This is necessary when importing config/install.
     /** @var \Drupal\Core\Entity\EntityDisplayRepository $entity_display_repository */
@@ -56,6 +55,62 @@ final class AddYmlRepoTest extends BrowserTestBase {
     $entity_display_repository->getFormDisplay('user', 'user', 'default')
       ->setComponent('field_repository_url', ['type' => 'link_default'])
       ->save();
+  }
+
+  /**
+   * Test that the settings page can be reached and works as expected.
+   *
+   * This tests that an admin user can access the settings page, select a
+   * plugin to enable, and submit the page successfully.
+   *
+   * @return void
+   *   Returns nothing.
+   *
+   * @test
+   */
+  public function testSettingsPage(): void {
+    // Get a handle on the browsing session.
+    $session = $this->assertSession();
+
+    // Navigate to the settings page.
+    $this->drupalGet('/admin/config/services/repositories');
+
+    // Confirm that page loads without error.
+    $session->statusCodeEquals(200);
+
+    // Set the value of form elements to be submitted.
+    $edit = ['edit-repositories-plugins-yml-remote' => 'yml_remote'];
+
+    // Submit the form.
+    $this->submitForm($edit, 'Save configuration');
+    $session->statusCodeEquals(200);
+
+    // Ensure the confirmation message appears.
+    $session->responseContains('The configuration options have been saved.');
+
+    $session->checkboxChecked('edit-repositories-plugins-yml-remote');
+    $session->checkboxNotChecked('edit-repositories-plugins-github');
+  }
+
+  /**
+   * Test that the settings page cannot be reached without permission.
+   *
+   * @return void
+   *   Returns nothing.
+   *
+   * @test
+   *
+   * @throws \Behat\Mink\Exception\ExpectationException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function testUnprivilegedSettingsPage(): void {
+    $session = $this->assertSession();
+    $authenticated_user = $this->drupalCreateUser(['access content']);
+    $this->drupalLogin($authenticated_user);
+    $this->drupalGet('/admin/config/services/repositories');
+    // Test to ensure that the page loads without error.
+    // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+    $session->statusCodeEquals(403);
   }
 
   /**
